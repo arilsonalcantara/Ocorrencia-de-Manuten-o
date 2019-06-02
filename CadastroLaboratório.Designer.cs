a4cd38e3576e4ed9cc0abc49076e80d73a98126a
Ana Enock <anaoenock@gmail.com>
2019-06-01 22:04:42 -0300

    Versão 1.2.3 Inserir a classe Laboratório e criei o Formulário de Cadastro de laboratório.

---
+++ CadastroLaboratório.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Ocorrencia_de_Manutenção
+{
+    partial class CadastroLaboratório
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSala = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAdm
+            // 
+            this.txtAdm.BackColor = System.Drawing.SystemColors.Window;
+            this.txtAdm.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtAdm.Location = new System.Drawing.Point(187, 57);
+            this.txtAdm.Name = "txtAdm";
+            this.txtAdm.Size = new System.Drawing.Size(185, 20);
+            this.txtAdm.TabIndex = 0;
+            this.txtAdm.Text = "Administrador";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Admnistrador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Código do Laboratório";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(187, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Código";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtStatus.Location = new System.Drawing.Point(187, 163);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(185, 20);
+            this.txtStatus.TabIndex = 5;
+            this.txtStatus.Text = "Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sala";
+            // 
+            // txtSala
+            // 
+            this.txtSala.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtSala.Location = new System.Drawing.Point(187, 225);
+            this.txtSala.Name = "txtSala";
+            this.txtSala.Size = new System.Drawing.Size(185, 20);
+            this.txtSala.TabIndex = 9;
+            this.txtSala.Text = "  ";
+            // 
+            // CadastroLaboratório
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 336);
+            this.Controls.Add(this.txtSala);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdm);
+            this.Name = "CadastroLaboratório";
+            this.Text = "CadastroLaboratório";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSala;
+    }
+}
---
+++ Laboratório.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocorrencia_de_Manutenção
+{
+    class Laboratório
+    {
+        private string _Adiministrador; // Pensei que cada laboratório tem um responsável, logo um administrador.
+
+        public string Administrador
+        {
+            get { return _Adiministrador; }
+            set { _Adiministrador = value; }
+        }
+
+        private string _Código; // Cada laboratório possui um identificador que é o código.
+
+        public string Código
+        {
+            get { return _Código; }
+            set { _Código = value; }
+        }
+
+        // Pensei que isso seria útil para a listagem, 
+        //porque ele pode querer listar os laboratórios e saber qual tá disponíivel e qual tá em manutenção.
+        // Ou modificar o status do laboratório depois. Podemos criar esse botão no Menu Principal
+
+        private string _Status;
+
+        public string Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+
+        //Coloquei sala para idenficar o local que o laboratório se encontra.
+
+        private string _Sala;
+
+        public string Sala
+        {
+            get { return _Sala; }
+            set { _Sala = value; }
+        }
+
+        //Pensei em colocar código do usuário também lá na tela.
+
+    }
+}
