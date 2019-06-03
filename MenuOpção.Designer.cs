0daff2f2176faed7ecb38d954c8278b28223d780
Arilson <Arilson@ARILSON-PC>
2019-06-03 19:35:33 -0300

    Versão 1.7.11 - Novo MenuOpção

--- MenuOpção.Designer.cs
+++ MenuOpção.Designer.cs
@@ -34,6 +34,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblUser = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +47,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button1.Location = new System.Drawing.Point(116, 28);
+            this.button1.Location = new System.Drawing.Point(117, 75);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 34);
             this.button1.TabIndex = 0;
@@ -59,9 +61,9 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button2.Location = new System.Drawing.Point(116, 88);
+            this.button2.Location = new System.Drawing.Point(96, 130);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 34);
+            this.button2.Size = new System.Drawing.Size(105, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Cadastrar Laboratório";
             this.button2.UseVisualStyleBackColor = true;
@@ -72,11 +74,11 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button3.Location = new System.Drawing.Point(116, 152);
+            this.button3.Location = new System.Drawing.Point(117, 196);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(161, 33);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Cadastrar Usuário";
+            this.button3.Text = "Usuarios do Sistema";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
@@ -93,21 +95,48 @@
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.lblUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblUser.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblUser.Font = new System.Drawing.Font("Verdana", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUser.Location = new System.Drawing.Point(8, 7);
+            this.lblUser.Location = new System.Drawing.Point(149, 30);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(89, 19);
             this.lblUser.TabIndex = 3;
             this.lblUser.Text = "Username";
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button4.Location = new System.Drawing.Point(201, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Laborátorios do Sistema";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Bem Vindo";
             // 
             // MenuOpção
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(380, 233);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -128,5 +157,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
